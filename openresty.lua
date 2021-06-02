b0ec19dd5d7209654175119e1112825bfb4004b5
Jacusss <83212643+Jacusss@users.noreply.github.com>
2021-06-02 20:16:04 +0200

    Update openresty.lua

--- openresty.lua
+++ openresty.lua
@@ -10,10 +10,21 @@ location = /lua_api_request {
       ngx.log(ngx.ERR, "request failed: ", err)
       return
     end
+        ngx.say(res.body,"\n")
     local t = cjson.decode(res.body)
-    ngx.say(type(t))
-    for k, v in pairs(t) do
-      print(k, v)
+        ngx.say("type: ",type(t))
+        a=(t.main.temp)
+        b=(t.main.pressure)
+        c=(t.main.humidity)
+        delta = (math.pow(b, 2) - ((4 * a) * c))
+        ngx.say("delta: ",delta)
+
+        ngx.say("type: ",type(a, b, c))
+        ngx.say("temp: ", a ," pressure: ", b ," humidity: ", c)
+
+    if delta>0 then
+     local  res, err = httpc:request_uri("192.168.2.2/win&A=200&FX=9", 
+        {method = "POST"})
     end
     }
     }
